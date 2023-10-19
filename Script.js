10c13c305d97a025c2c43d5000f31cdae4aba3b0
Alexis072002 <alexis.dezeque@outlook.com>
2023-10-19 15:46:39 +0200

    changes newsletter html file

--- Script.js
+++ Script.js
@@ -3,3 +3,8 @@ fetch("homepage.html")
 .then(data => {
     document.querySelector("header").innerHTML = data;
 })
+fetch("footer.html")
+.then(response => response.text())
+.then(data => {
+    document.querySelector("footer").innerHTML = data;
+})
