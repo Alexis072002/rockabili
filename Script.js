aa84e3b46e62eb4fe2d7372b304a31d673c5f792
Alexis072002 <alexis.dezeque@outlook.com>
2023-10-20 10:39:29 +0200

    add contactforms in the JS script

--- Script.js
+++ Script.js
@@ -14,3 +14,8 @@ fetch("footer.html")
 .then(data => {
     document.querySelector("footer").innerHTML = data;
 })
+fetch("contactforms.html")
+.then(response => response.text())
+.then(data => {
+    document.querySelector("main").innerHTML = data;
+})
